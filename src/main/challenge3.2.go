a2e075e28e0684cdc63d005635594800ab789b1d
Rafif Taris <rafiftaris1@gmail.com>
2019-06-12 16:04:37 +0700

    challenge 3.2

---
+++ src/main/challenge3.2.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func strToIntArr(str string) (result []int){
+	var k int
+	arrStr := strings.Split(str,",")
+	for _,each := range arrStr{
+		k,_=strconv.Atoi(each)
+		result = append(result,k)
+	}
+	return
+}
+
+func TwoSums(Nums []int, Sum int) (result [][]int){
+	for i:=0 ; i<len(Nums) ; i++{
+		for j:=i+1 ; j<len(Nums) ; j++{
+			if Nums[i] + Nums[j] == Sum{
+				result = append(result, []int{Nums[i], Nums[j]})
+			}
+		}
+	}
+	return
+}
+
+func main()  {
+	var nums string
+	var sum int
+	fmt.Println("Input: ")
+	fmt.Print("Nums = ")
+	_,_ = fmt.Scanln(&nums)
+	fmt.Print("Sum = ")
+	_,_ = fmt.Scanln(&sum)
+
+	arrNums := strToIntArr(nums)
+
+	fmt.Print("Output: ")
+	fmt.Println(TwoSums(arrNums,sum))
+
+
+}
+
