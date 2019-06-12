c0354f55f092dca0a18423870bfaf5e5ca7b59d7
Rafif Taris <rafiftaris1@gmail.com>
2019-06-12 13:39:10 +0700

    challenge 1.3

---
+++ src/main/challenge1.3.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func factor(num int) (result []int) {
+	for i:=1 ; i<num/2 ; i++{
+		if num%i==0{
+			result = append(result, i)
+		}
+	}
+
+	result = append(result,num)
+
+	return
+}
+
+func printSlice(s []int)  {
+	for _,each := range s{
+		fmt.Println(each)
+	}
+}
+
+func main()  {
+	var num int
+	fmt.Print("Input : ")
+	_, _ = fmt.Scanln(&num)
+
+	fmt.Println("Output :")
+	printSlice(factor(num))
+}
