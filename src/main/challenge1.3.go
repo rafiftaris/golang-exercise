9ae4de8c64615697b08d05fca87074d8b71d4df6
Rafif Taris <rafiftaris1@gmail.com>
2019-06-13 13:18:50 +0700

    fix challenge 1.3

--- src/main/challenge1.3.go
+++ src/main/challenge1.3.go
@@ -5,24 +5,24 @@ import (
 )
 
 func factor(num int) (result []int) {
-	for i:=1 ; i<num/2 ; i++{
-		if num%i==0{
+	for i := 1; i <= num/2; i++ {
+		if num%i == 0 {
 			result = append(result, i)
 		}
 	}
 
-	result = append(result,num)
+	result = append(result, num)
 
 	return
 }
 
-func printSlice(s []int)  {
-	for _,each := range s{
+func printSlice(s []int) {
+	for _, each := range s {
 		fmt.Println(each)
 	}
 }
 
-func main()  {
+func main() {
 	var num int
 	fmt.Print("Input : ")
 	_, _ = fmt.Scanln(&num)
