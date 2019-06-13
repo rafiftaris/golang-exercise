0b0c8c2fd3ecc637f20ee2da4849024d7399c18c
Rafif Taris <rafiftaris1@gmail.com>
2019-06-13 13:35:44 +0700

    fix challenge 2.1

--- src/main/challenge2.1.go
+++ src/main/challenge2.1.go
@@ -2,31 +2,32 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
 )
 
 func countMean(arr []int) int {
-	var sum int
-	for _,each:=range arr{
-		sum += each
+	var sum float64
+	for _, each := range arr {
+		sum += float64(each)
 	}
-	return sum/len(arr)
+	mean := sum / float64(len(arr))
+	return int(math.Round(mean))
 }
 
-func main()  {
+func main() {
 	var numbers string
-	fmt.Print("Input: ")
+	fmt.Print("Input (contoh:1,2,3,4,5): ")
 	_, _ = fmt.Scanln(&numbers)
 
-	split := strings.Split(numbers,",")
+	split := strings.Split(numbers, ",")
 	var arrNum []int
-	for _,each := range split{
-		num,_:=strconv.Atoi(each)
-		arrNum = append(arrNum,num)
+	for _, each := range split {
+		num, _ := strconv.Atoi(each)
+		arrNum = append(arrNum, num)
 	}
 
 	fmt.Print("Output: ")
 	fmt.Println(countMean(arrNum))
 }
-
