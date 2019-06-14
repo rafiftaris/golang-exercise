95d09ec9d80ad080e482b5df377f1d51c18c40d9
Rafif Taris <rafiftaris1@gmail.com>
2019-06-14 10:57:16 +0700

    handle unexpected user inputs

--- src/main/challenge2.3.go
+++ src/main/challenge2.3.go
@@ -7,15 +7,32 @@ import (
 
 func main() {
 	var keys, values string
+	var keysArr, valuesArr []string
 	fmt.Println("CONVERT CSV TO MAP")
-	fmt.Println("Input: ")
-	fmt.Print("keys (ex:name,age)= ")
-	_, _ = fmt.Scanln(&keys)
-	fmt.Print("values (ex:Aang,12)= ")
-	_, _ = fmt.Scanln(&values)
 
-	keysArr := strings.Split(keys, ",")
-	valuesArr := strings.Split(values, ",")
+	for {
+		fmt.Println("Input: ")
+		fmt.Print("keys (ex:name,age)= ")
+		_, _ = fmt.Scanln(&keys)
+		fmt.Print("values (ex:Aang,12)= ")
+		_, _ = fmt.Scanln(&values)
+
+		keysArr = strings.Split(keys, ",")
+		valuesArr = strings.Split(values, ",")
+
+		if keys == "" || values == ""{
+			fmt.Println("Input tidak boleh kosong")
+			fmt.Println()
+			continue
+		}
+		if len(keysArr) != len(valuesArr){
+			fmt.Println("Masukkan keys dan value dengan jumlah yang sama")
+			fmt.Println()
+			continue
+		}
+
+		break
+	}
 
 	var result = make(map[string]string)
 
